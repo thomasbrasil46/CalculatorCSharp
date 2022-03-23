05efb82db79ef2e0a9e289eed254a2a56889e27e
thomasbrasil46 <thomasbrasilpb@gmail.com>
2022-03-23 18:38:35 -0300

    Using functions.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,12 @@ namespace CalculatorCSharp
     {
         static void Main(string[] args)
         {
-           Console.Clear();
+           soma();
+        }
+
+        static void soma() 
+        {
+            Console.Clear();
 
             Console.WriteLine("Primeiro Valor: ");
             float v1 = float.Parse( Console.ReadLine());
@@ -19,11 +24,7 @@ namespace CalculatorCSharp
             float resultado = v1 + v2;
             Console.WriteLine("O resultado da soma é: " + resultado );
 
-            /* outras formas de soma:
-            Console.WriteLine($"O resultado da soma é {resultado}");
-            Console.WriteLine($"O resultado da soma é {v1 + v2}");
-            Console.WriteLine("O resultado da soma é " + (v1 + v2));
-            */
+           
         }
     }
 }
