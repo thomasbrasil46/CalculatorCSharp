0f0a3a16a97ca19fdc610ae0433bef8165348dc9
thomasbrasil46 <thomasbrasilpb@gmail.com>
2022-03-23 22:49:29 -0300

    Exiting the application.

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ namespace CalculatorCSharp
             Console.WriteLine("2 - Subtração");
             Console.WriteLine("3 - Divisão");
             Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
 
             Console.WriteLine("-----------------");
             Console.WriteLine("Selecione uma opção: ");
@@ -30,6 +31,7 @@ namespace CalculatorCSharp
                 case 2: Subtracao(); break;
                 case 3: Divisao(); break;
                 case 4: Multiplicacao(); break;
+                case 5: System.Environment.Exit(0); break;
                 default: Menu(); break;
             }
         }
